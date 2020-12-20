ffbc989e7c3571822819d0b251b4dc8743a389ef
Pavle Eftimovski <pavle.eftimovski@olmero.ch>
2020-12-20 19:54:57 +0100

    REN-XXX: improve integration tests

---
+++ src/test/java/com/exercise/BaseIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.exercise;
+
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+		webEnvironment = RANDOM_PORT,
+		properties = "spring.main.allow-bean-definition-overriding=true")
+public class BaseIntegrationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@LocalServerPort
+	private int port;
+
+	@Before
+	public void setUp() {
+		RestAssured.port = port;
+	}
+
+}
